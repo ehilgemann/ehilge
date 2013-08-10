134d0a45047f1cd33baadb9c2eb5ab1ad34cc4fe
migreyes <github@migreyes.com>
2013-08-10 15:54:58 -0500

    Update analytics and .gitignore

--- assets/javascript/latest-tweet.js
+++
@@ -1,8 +0,0 @@
-getTwitters('tweet', {
-  id: 'migreyes',
-  count: 1,
-  enableLinks: true,
-  ignoreReplies: true,
-  clearContents: true,
-  template: '%text% <span style="color: #CCC;">&ndash;</span> <a href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
-});

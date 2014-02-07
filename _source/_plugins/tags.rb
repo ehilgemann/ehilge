3410a6f914c97c23e67cc284e9d02416781b8724
migreyes <mig@37signals.com>
2014-02-07 00:45:47 -0600

    Add support for non-jpg argument.

--- _source/_plugins/tags.rb
+++ _source/_plugins/tags.rb
@@ -3,12 +3,18 @@ module Jekyll
     def initialize(tag_name, project, tokens)
       super
       arguments = project.split(",")
-      @path    = arguments[0].strip
-      @project = arguments[1].strip
+      @path     = arguments[0].strip
+      @project  = arguments[1].strip
+
+      if arguments.size > 2
+        @filetype = arguments[2].strip
+      else
+        @filetype = "jpg"
+      end
     end
 
     def render(context)
-      %|<img src="/assets/images/makes/#{@path}/#{@project}.jpg" retina="/assets/images/makes/#{@path}/#{@project}@2x.jpg">|
+      %|<img src="/assets/images/makes/#{@path}/#{@project}.#{@filetype}" retina="/assets/images/makes/#{@path}/#{@project}@2x.#{@filetype}">|
     end
 
     Liquid::Template.register_tag "project", self
